ab263fa6ed3c6be6e38fd1ae86fba3d7337c69e3
daydreamer <s419922572@gmail.com>
2023-09-29 23:00:28 +0900

    updated prompt with roles

--- backend/app.py
+++ backend/app.py
@@ -15,24 +15,27 @@ def generate_email():
     # Get data from POST request
     data = request.json
 
-    # Use your 日本語メール作成AI logic here
-    email_content = "以下の内容に基づいてメールの本文を作成してください。拝啓と敬具は不要、署名は直接書く署名：は不要。宛名：{宛名の内容 or '' if none}要件：{要件の内容}{署名の内容 or '' if none}"
+    # system_content
+    sys_content = "以下の内容(署名,宛名,要件)に基づいてメールの本文を作成してください。'拝啓'と'敬具'は不要.署名と宛名が不明な場合は分かりやすく'「署名」'と「'宛名」'と書く。"
 
-    # email_content concat with data
-    full_prompt = email_content + "\n" + "宛名:" + data['recipient'] + "\n" + "署名:" + data['signature'] + "\n" + "要件:" + data['text']
+    # user_content
+    user_content = "宛名:" + data['recipient'] + "\n" + "署名:" + data['signature'] + "\n" + "要件:" + data['text']
 
     # send to openai api gpt3.5turbo and get response
-    response = chatgpt(full_prompt)
+    response = chatgpt(sys_content, user_content)
 
     return jsonify({"result": response})
 
 
-def chatgpt(text):
+def chatgpt(sys_content, user_content):
     try:
         completion = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
-                {"role": "user", "content": text}
+                {"role": "system",
+                 "content": sys_content},
+                {"role": "user",
+                 "content": user_content}
             ]
         )
     except:
